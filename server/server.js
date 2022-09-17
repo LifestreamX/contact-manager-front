2e0ea72679db0c4749274d991add54d16c98d51a
haloandfable <tylerallen@live.com>
2022-09-17 14:29:46 -0400

    make it better

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('.db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('./db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
 
 const port = process.env.PORT || 9000; // <== You can change the port
 server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
 server.use(router);
-server.use(router);
-
-server.listen(port);
+server.listen(port, () => console.log(`server is running on  ${port}`));
