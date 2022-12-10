cb71895cf240b3e20a7a87d6d921600ac6c20bdb
haloandfable <tylerallen@live.com>
2022-12-10 13:09:45 -0500

    revert

--- src/App.js
+++ src/App.js
@@ -24,10 +24,6 @@ function App() {
   const [relationship, setRelationship] = useState('');
   const [relationShipInfo, setRelationshipInfo] = useState([]);
 
-  var http = require('http');
-  setInterval(function () {
-    http.get('https://contactmanager.tyler-allen.com');
-  }, 300000); // every 5 minutes (300000)
 
   return (
     <div className='app-wrapper'>
