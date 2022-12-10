c56fee2e9642ef296197ffa221478770d816fea4
haloandfable <tylerallen@live.com>
2022-12-10 14:00:28 -0500

    m

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ function App() {
   const [relationShipInfo, setRelationshipInfo] = useState([]);
 
 
+  var http = require('http');
+setInterval(function () {
+  http.get('https://contact-manager-back.onrender.com');
+}, 300000); // every 5 minutes (300000)
+
+
+
   return (
     <div className='app-wrapper'>
       <NavBar />
