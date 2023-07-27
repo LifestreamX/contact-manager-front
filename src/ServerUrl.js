22a950a670be4907d30e24ad98f4179395f9a707
haloandfable <tylerallen@live.com>
2023-07-27 13:07:13 -0400

    url update

--- src/ServerUrl.js
+++ src/ServerUrl.js
@@ -1,3 +1,13 @@
+
+
+
 export const productionServerUrl = process.env.REACT_APP_BACKEND_SERVER_URL;
+
+
+
+// export const productionServerUrl = process.env.REACT_APP_DEV_SERVER;
+
+
+
 
 export const devServerUrl = process.env.REACT_APP_DEV_SERVER;
